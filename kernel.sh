da2560a6ead7aabf9ee4989f7619f1883a8e03ae
Andres <59713551+afcaballero-1994@users.noreply.github.com>
2025-02-27 10:03:41 -0500

    updated to new nvidia driver 570.124.04

--- kernel.sh
+++ kernel.sh
@@ -33,19 +33,21 @@ kver=$(cd /usr/lib/modules && echo * | awk '{print $NF}')
 
 dracut -f --kver=$kver --reproducible --zstd --no-hostonly /usr/lib/modules/$kver/initramfs.img $kver
 
+NVIDIA_DRIVER_VERSION="570.124.04"
+
 wget \
-    https://us.download.nvidia.com/XFree86/Linux-x86_64/570.86.16/NVIDIA-Linux-x86_64-570.86.16.run
+    https://us.download.nvidia.com/XFree86/Linux-x86_64/$NVIDIA_DRIVER_VERSION/NVIDIA-Linux-x86_64-$NVIDIA_DRIVER_VERSION.run
 
-chmod +x NVIDIA-Linux-x86_64-570.86.16.run
+chmod +x NVIDIA-Linux-x86_64-$NVIDIA_DRIVER_VERSION.run
 
 
-/NVIDIA-Linux-x86_64-570.86.16.run\
+/NVIDIA-Linux-x86_64-$NVIDIA_DRIVER_VERSION.run\
     --silent --run-nvidia-xconfig --dkms \
     --kernel-source-path /usr/src/kernels/$kver \
     --kernel-module-type=proprietary \
     --kernel-name=$kver
 
-rm -f /NVIDIA-Linux-x86_64-570.86.16.run
+rm -f /NVIDIA-Linux-x86_64-$NVIDIA_DRIVER_VERSION.run
 dracut -vf --kver=$kver --reproducible --zstd --no-hostonly /usr/lib/modules/$kver/initramfs.img $kver
 
 dnf remove $(dnf repoquery --installonly --latest-limit=-1 -q) -y
